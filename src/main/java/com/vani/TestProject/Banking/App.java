26f61e17610604f587a619570cb821b09c167e8c
samvijay1987 <95726056+samvijay1987@users.noreply.github.com>
2021-12-08 18:34:21 +0530

    Update App.java

--- src/main/java/com/vani/TestProject/Banking/App.java
+++ src/main/java/com/vani/TestProject/Banking/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        <properties>
+    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+  </properties>
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
